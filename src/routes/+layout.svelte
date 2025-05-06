3e11b08951befe94174ac0dc44fb1101ef4ead22
Dratatin <matchirouze@orange.fr>
2025-05-06 22:39:07 +0200

    Implement Lenis for smooth scroll and change svelte config for static build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script lang="ts">
 	import "../app.css";
+	import "lenis/dist/lenis.css";
+	import { onMount } from "svelte";
+	import Lenis from "lenis";
+
+	onMount(() => {
+		const lenis = new Lenis({
+			autoRaf: true
+		});
+
+		// Listen for the scroll event and log the event data
+		lenis.on("scroll", () => {
+			// console.log(e);
+		});
+
+		return () => {
+			lenis.destroy();
+		};
+	});
 
 	let { children } = $props();
 </script>
